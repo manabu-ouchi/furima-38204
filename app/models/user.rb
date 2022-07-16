b3c0f00a6047af4696231ad0887ed9ac0bd9df1d
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-07-16 15:54:24 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: true
+  validates :profile, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_katakana, presence: true
+  validates :last_name_katakana, presence: true
+  validates :birth_date, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/i.freeze
+  validates :first_name, :last_name, format: { with: VALID_PASSWORD_REGEX }
+  VALID_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/i.freeze
+  validates :first_name_katakana, :last_name_katakana, format: { with: VALID_LATAKANA_REGEX }
+ 
+  
   has_many :items
   has_many :orders
          
