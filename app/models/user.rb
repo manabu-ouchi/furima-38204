c4bbd93f35a4ff7b14d2263c12ea98492bddcfcc
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-07-16 18:22:37 +0900

    エラーが出たので一度正規表現を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,7 @@ class User < ApplicationRecord
   validates :last_name_katakana, presence: true
   validates :birth_date, presence: true
 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/i.freeze
-  validates :first_name, :last_name, format: { with: VALID_PASSWORD_REGEX }
-  VALID_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/i.freeze
-  validates :first_name_katakana, :last_name_katakana, format: { with: VALID_KATAKANA_REGEX }
+  
  
   
   has_many :items
