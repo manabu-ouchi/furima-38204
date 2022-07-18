412510fcfe5e89bbce56df0aedb6d66d59005720
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-07-18 19:03:50 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :detail, presence: true
+  validates :price, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :delivery_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :delivery_day_id, numericality: { other_than: 1, message: "can't be blank" } 
+
+  
 end
