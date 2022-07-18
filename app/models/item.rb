156a8041db97895da91f6e5b840fca62db8c22f8
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-07-18 22:32:31 +0900

    アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery
+  belongs_to :prefecture
+  belongs_to :delivery_day
+
 
   validates :image, presence: true
   validates :name, presence: true
