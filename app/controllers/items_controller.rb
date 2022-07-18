d1d7cc32c7c2c39b9dd35b15e8062504d3d1b8c5
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-07-19 00:27:02 +0900

    コントローラー記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,23 @@
 class ItemsController < ApplicationController
+  def index
+  end
+  
+  def new
+    @item = Item.new
+  
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      
+      render :new
+    end
+  end
+  private
+  def item_params
+    params.require(:item).permit(:name, :detail, :price, :category_id, :condition_id, :delivery_id, :prefecture_id, :delivery_day_id, :image).merge(user_id: current_user.id)
+  end
 end
