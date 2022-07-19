d86e91e7e3ff8d017d179834bc78c75f82962e17
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-07-19 18:16:24 +0900

    プルリクエストの修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,11 +2,10 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = priceInput.value * 0.1
+    addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1)
     const profit = document.getElementById("profit");
-    profit.innerHTML =  priceInput.value - priceInput.value * 0.1
+    profit.innerHTML =  Math.floor(priceInput.value - priceInput.value * 0.1)
 
 
   })
