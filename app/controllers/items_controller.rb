063a39af6c68926a48b173d476ead0a75f49ff1e
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-07-20 14:22:08 +0900

    一覧表示できるように実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,9 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!,only: [:new]
 
   def index
+    @items = Item.includes(:user).order("created_at DESC")
+    
+
   end
   
   def new
