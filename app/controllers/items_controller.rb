8ac5a04af44351e37023944bbc0263779d685e22
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-07-21 12:20:30 +0900

    before_actionに纏める

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -20,15 +21,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+
   end
 
-  def edit
-    @item = Item.find(params[:id])
+  def edit  
+    
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
 
@@ -52,5 +52,9 @@ class ItemsController < ApplicationController
     unless user_signed_in? && current_user.id == item.user_id 
       redirect_to root_path
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
