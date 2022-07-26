1f57a72519dc425c3193769dc23998976405ac32
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-07-27 02:54:20 +0900

    未ログインで購入画面のパスを入力してもログイン画面に遷移するように記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,10 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
   def index
   
     @item = Item.find(params[:item_id])
     @order_delivery_address = OrderDeliveryAddress.new
+    
 
 
   end
