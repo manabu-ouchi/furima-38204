eadcbd8e491290c51a0009142caf4f1dd0d80195
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-07-26 17:39:32 +0900

    決済機能を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,9 +8,11 @@ class OrdersController < ApplicationController
   end
 
   def create 
-    @order_delivery_address = OrderDeliveryAddress.new(order_params)
-    binding.pry
+    @item = Item.find(params[:item_id])
+    @order_delivery_address = OrderDeliveryAddress.new(order_params) 
     if @order_delivery_address.valid?
+      pay_item
+     
        @order_delivery_address.save    
        redirect_to root_path
     else
@@ -22,7 +24,17 @@ class OrdersController < ApplicationController
   
 
   def order_params
-    params.require(:order_delivery_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id] )
+    params.require(:order_delivery_address).permit(:post_code, :prefecture_id, :city, :address, :building_name,
+                                                   :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token] )
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_103ee32d6aef03a685ac7cb1"
+    Payjp::Charge.create(
+      amount: @item.price,  
+      card: order_params[:token],    
+      currency: 'jpy'                
+    )
   end
 
           
