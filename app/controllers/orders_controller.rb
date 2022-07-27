4c0ae4995c2f9f8a282fdfd48ec7884989e51edb
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-07-27 14:50:37 +0900

    befor_actionに纏める

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,13 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_order, only: [:index, :create]
+  
   def index
-    @item = Item.find(params[:item_id])
     @order_delivery_address = OrderDeliveryAddress.new
     redirect_to root_path if @item.user_id == current_user.id || @item.order.present?
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_delivery_address = OrderDeliveryAddress.new(order_params)
     if @order_delivery_address.valid?
       pay_item
@@ -20,6 +20,9 @@ class OrdersController < ApplicationController
   end
 
   private
+  def set_order
+    @item = Item.find(params[:item_id])
+  end
 
   def order_params
     params.require(:order_delivery_address).permit(:post_code, :prefecture_id, :city, :address, :building_name,
